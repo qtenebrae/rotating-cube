d4aa8baaf6617d94f3adc5d051a09b3e8f4d2e8f
pxtheticcat <serega.ivanovv2012@gmail.com>
2023-11-04 21:49:55 +0300

    Impement a basic shader program
    Designed to create a square grid using specified vertices and colours
    It consists of two parts vertex shaders and fragment shaders
    
    Vertex Shaders are used to transform the vertices of models. Vertex shaders perform operations such as scaling, rotation, displacement, and perspective transformations to determine the final position of each vertex in 3D space
    
    Fragment Shaders, also known as pixel shaders, work at the level of individual fragments that represent pixels on the screen. They determine the final colour of each fragment, and can use information from vertex shaders and other data to apply lighting, texturing, effects, or other operations

---
+++ scene.py
@@ -0,0 +1,15 @@
+from settings import *
+import moderngl as mgl
+from meshes.quad_mesh import QuadMesh
+
+
+class Scene:
+    def __init__(self, app):
+        self.app = app
+        self.quad = QuadMesh(self.app)
+
+    def update(self):
+        pass
+
+    def render(self):
+        self.quad.render()
